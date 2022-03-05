9186349e300116c2ed654cacd0a592b29c47938b
Flávio Tassan <ftassan@outlook.com>
2022-03-05 11:59:28 -0300

    Testes unitários do Cupom

--- CocoaStore.Vendas.Domain/Descontos/Cupom.cs
+++ CocoaStore.Vendas.Domain/Descontos/Cupom.cs
@@ -1,4 +1,5 @@
 ﻿using CocoaStore.Vendas.Domain.Core.Entidades;
+using CocoaStore.Vendas.Domain.Estoque;
 
 namespace CocoaStore.Vendas.Domain.Descontos;
 
@@ -6,4 +7,21 @@ public class Cupom : Entidade
 {
     public string Codigo { get; set; }
     public double Desconto { get; set; }
+    public List<Produto> ProdutosLiberados { get; set; }
+    public List<Produto> ProdutosBloqueados { get; set; }
+
+    public Cupom(string codigo, double desconto)
+    {
+        Codigo = codigo;
+        Desconto = desconto;
+        ProdutosLiberados = new List<Produto>();
+        ProdutosBloqueados = new List<Produto>();
+    }
+
+    public void AdicionarProdutoLiberado(Produto produto) => ProdutosLiberados.Add(produto);
+    public void AdicionarProdutoBloqueado(Produto produto) => ProdutosBloqueados.Add(produto);
+    public void RemoverProdutoLiberado(Produto produto) => ProdutosLiberados.Remove(produto);
+    public void RemoverProdutoBloqueado(Produto produto) => ProdutosBloqueados.Remove(produto);
+    public bool EhProdutoLiberado(Produto produto) => ProdutosLiberados.Contains(produto);
+    public bool EhProdutoBloqueado(Produto produto) => ProdutosBloqueados.Contains(produto);
 }
---
+++ CocoaStore.Vendas.Unit.Tests/Descontos/CupomUnitTests.cs
@@ -0,0 +1,93 @@
+﻿using CocoaStore.Vendas.Domain.Descontos;
+using CocoaStore.Vendas.Unit.Tests.Config.Fixtures;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CocoaStore.Vendas.Unit.Tests.Descontos;
+
+public class CupomUnitTests : IClassFixture<ProdutoFixture>
+{
+    private readonly ProdutoFixture _fixture;
+    private readonly ITestOutputHelper _outputHelper;
+
+    public CupomUnitTests(ProdutoFixture fixture, ITestOutputHelper outputHelper)
+    {
+        _fixture = fixture;
+        _outputHelper = outputHelper;
+    }
+
+    [Fact]
+    public void Deve_CriarCupom()
+    {
+        var cupom = new Cupom("SOHOJE", 150);
+
+        cupom.Codigo
+            .Should()
+            .NotBeNull();
+
+        cupom.Desconto
+            .Should()
+            .NotBe(0);
+
+        cupom.ProdutosLiberados
+            .Should()
+            .NotBeNull();
+        
+        cupom.ProdutosBloqueados
+            .Should()
+            .NotBeNull();
+    }
+
+    [Fact]
+    public void Deve_AdicionarProduto_A_ListaDeLiberados()
+    {
+        var cupom = new Cupom("100OFF", 100);
+        var produto = _fixture.GerarProdutoValido();
+
+        cupom.AdicionarProdutoLiberado(produto);
+
+        cupom.EhProdutoLiberado(produto)
+            .Should()
+            .BeTrue();
+    }
+
+    [Fact]
+    public void Deve_AdicionarProduto_A_ListaDeBloqueados()
+    {
+        var cupom = new Cupom("100OFF", 100);
+        var produto = _fixture.GerarProdutoValido();
+
+        cupom.AdicionarProdutoBloqueado(produto);
+
+        cupom.EhProdutoBloqueado(produto)
+            .Should()
+            .BeTrue();
+    }
+    
+    [Fact]
+    public void Deve_RemoverProduto_Da_ListaDeLiberados()
+    {
+        var cupom = new Cupom("100OFF", 100);
+        var produto = _fixture.GerarProdutoValido();
+
+        cupom.RemoverProdutoLiberado(produto);
+
+        cupom.EhProdutoLiberado(produto)
+            .Should()
+            .BeFalse();
+    }
+
+    [Fact]
+    public void Deve_RemoverProduto_Da_ListaDeBloqueados()
+    {
+        var cupom = new Cupom("100OFF", 100);
+        var produto = _fixture.GerarProdutoValido();
+
+        cupom.RemoverProdutoBloqueado(produto);
+
+        cupom.EhProdutoBloqueado(produto)
+            .Should()
+            .BeFalse();
+    }
+}
