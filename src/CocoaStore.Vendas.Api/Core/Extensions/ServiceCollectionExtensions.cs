75c3e64b2c1d925a81571bca14e82cb0ae1bfb4b
Flávio Tassan <ftassan@outlook.com>
2022-03-05 13:11:49 -0300

    Registrando os serviços do Mongo e Repositorios

---
+++ src/CocoaStore.Vendas.Api/Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CocoaStore.Vendas.Domain.Shared.Interfaces.Repositories.CarrinhoDeCompras;
+using CocoaStore.Vendas.Domain.Shared.Interfaces.Repositories.Estoque;
+using CocoaStore.Vendas.Infra.Contexts;
+using CocoaStore.Vendas.Infra.Interfaces;
+using CocoaStore.Vendas.Infra.Repository;
+using CocoaStore.Vendas.Infra.UoW;
+
+namespace CocoaStore.Vendas.Api.Core.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.MongoServices();
+        services.RepositoryServices();
+    }
+
+    private static void MongoServices(this IServiceCollection services)
+    {
+        services.AddScoped<IMongoContext, MongoContext>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+    private static void RepositoryServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProdutoRepository, ProdutoRepository>();
+        services.AddScoped<ICarrinhoRepository, CarrinhoRepository>();
+    }
+}
--- src/CocoaStore.Vendas.Api/Program.cs
+++ src/CocoaStore.Vendas.Api/Program.cs
@@ -1,4 +1,9 @@
+using CocoaStore.Vendas.Api.Core.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.RegisterServices();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
